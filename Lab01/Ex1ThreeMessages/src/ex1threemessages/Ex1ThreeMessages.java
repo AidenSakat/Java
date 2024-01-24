1f9ed0c8d764dd5bde8fe9bdeca963291a289355
AidenSakat <126438824+AidenSakat@users.noreply.github.com>
2024-01-23 22:11:26 -0800

    Add files via upload

---
+++ Lab01/Ex1ThreeMessages/src/ex1threemessages/Ex1ThreeMessages.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex1threemessages;
+
+/**
+ *
+ * @author User
+ */
+public class Ex1ThreeMessages {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is Aiden Sakat");
+        System.out.println("Welcome to CS-182");
+
+        // TODO code application logic here
+    }
+    
+}
---
+++ Lab01/Ex2Pattern/src/ex2pattern/Ex2Pattern.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex2pattern;
+
+/**
+ *
+ * @author User
+ */
+public class Ex2Pattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("      J       A      V     V      A   ");
+        System.out.println("      J      A A      V   V      A A  ");
+        System.out.println("J     J     AAAAA      V V      AAAAA  ");
+        System.out.println(" J   J     A     A      V      A     A  ");
+    }  
+    
+}
